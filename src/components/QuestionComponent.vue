5d516264c85df84e10379770a1aa310646d0b03c
Aleksandar Picuric <apicuric@gmail.com>
2020-08-06 23:53:57 +0200

    Question cmpt UI added
    Question cmpt UI added

---
+++ src/components/QuestionComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="panel panel-default">
+    <div class="panel-heading text-center">
+      <h3 >Question</h3>
+    </div>
+    <div class="panel-body">
+      <div class="col-xs-12 col-sm-6 text-center">
+        <button class="btn btn-primary btn-lg" style="margin: 10px" >1</button>
+      </div>
+      <div class="col-xs-12 col-sm-6 text-center">
+        <button class="btn btn-primary btn-lg" style="margin: 10px">1</button>
+      </div>
+      <div class="col-xs-12 col-sm-6 text-center">
+        <button class="btn btn-primary btn-lg" style="margin: 10px">1</button>
+      </div>
+      <div class="col-xs-12 col-sm-6 text-center">
+        <button class="btn btn-primary btn-lg" style="margin: 10px">1</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  .panel-heading{
+    padding: 8px 10px
+  }
+  h3{
+    margin-top: 10px;
+  }
+</style>
