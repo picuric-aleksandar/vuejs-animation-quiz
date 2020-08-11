cfcb3389a26af43471ef59a33f1608412f1467ee
Aleksandar Picuric <apicuric@gmail.com>
2020-08-11 14:11:43 +0200

    OnLoad and onClick animation added
    Added animations on app load and on clicking on the answer to animate switching between cmpts.

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,22 @@
             </div>
         </div>
         <hr>
-        <div class="row">
-          <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-            <component :is="activeCmpt" @answered="switchCmpt($event)" @confirmed="activeCmpt = 'app-question-cmpt'"> 
-            </component>
+        <!-- Transition na ceo red koji drzi nase komponente da bi se dobila animacija na ucitavanju
+              koristimo cetiri klase koje ispisuju stanje fade-nesto klase u <style> -->
+        <transition name="fade" appear mode="out-in">
+          <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+              <!-- Koristimo animaciju sa neta mozemo dodati name="" pa animaciju dodati kao dole ili iskoristiti klase
+                    kao u projektu vuejs-animations (pogledaj) -->
+              <transition
+                name="flip"
+                mode="out-in">
+                <component :is="activeCmpt" @answered="switchCmpt($event)" @confirmed="activeCmpt = 'app-question-cmpt'"> 
+                </component>
+              </transition>
+            </div>
           </div>
-        </div>
+        </transition>
     </div>
 </template>
 
@@ -22,7 +32,7 @@ import QuestionComponent from './components/QuestionComponent.vue';
   export default {
     data () {
       return {
-        activeCmpt: 'app-question-cmpt'
+        activeCmpt: null
       }
     },
     methods: {
@@ -44,35 +54,65 @@ import QuestionComponent from './components/QuestionComponent.vue';
       'app-correct-cmpt': CorrectComponent,
       'app-wrong-cmpt': WrongComponent,
       'app-question-cmpt': QuestionComponent,
+    },
+    created(){
+      this.activeCmpt = 'app-question-cmpt';
     }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.fade-enter {
+    opacity: 0;
+  }
+.fade-enter-active {
+  transition: opacity 1s;
+}
+.fade-leave {
+  /* opacity: 1; */
+}
+.fade-leave-active {
+  transition: opacity 1s;
+  opacity: 0;
 }
 
-h1, h2 {
-  font-weight: normal;
+
+/**flip animation prilikom load-ovanja */
+
+.flip-enter {
+  /* opacity: 0; */
+}
+.flip-enter-active {
+  /* animation: flip-in .3s ease-out forwards; */
+  animation: flipInX;
+  animation-duration: .5s;
+}
+.flip-leave {
+
+}
+.flip-leave-active{
+  animation: flipOutX;
+  animation-duration: .5s;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+@keyframes flip-out {
+  from {
+    transform: rotateX(0deg);
+  }
+  to {
+    transform: rotateX(90deg);
+  }
+}
+@keyframes flip-in {
+  from {
+    transform: rotateX(90deg);
+  }
+  to {
+    transform: rotateX(0deg);
+  }
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.animate__animated.animate__flipInX {
+  --animate-duration: .5s;
 }
 </style>
